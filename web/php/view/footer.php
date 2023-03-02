c5d61c42be808f1fbc13b5f89f0a91e16a9aece3
ChadOW <chad.estoup@gmail.com>
2023-03-02 11:31:51 +0100

    Add copyriths

--- web/php/view/footer.php
+++ web/php/view/footer.php
@@ -1,6 +1,6 @@
         </div>
         <footer class="inline center">
-            <p>Developped by <a href="https://chades.fr">Chad Estoup--Streiff</a></p>
+            <p>© 2023 EMA-Escalade | Developped by <a href="https://chades.fr">Chad Estoup--Streiff</a></p>
         </footer>
     </body>
 </html>
